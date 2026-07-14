1459b01dfebbf8f68986813066df48b9f4aa9341
Aluno <Aluno@172.16.2.77>
2026-07-14 11:53:13 -0300

    Class Usuario

--- src/colecionismo/Usuario.java
+++ src/colecionismo/Usuario.java
@@ -2,4 +2,28 @@ package colecionismo;
 
 public class Usuario {
 
+	private String nomeUsuario;
+	private String email;
+	private int dataNascimento;
+	private int telefone;
+	private String areaInteresse;
+	private ArrayList<Colecao> colecoes; 
+	private FotoPerfil;
+	
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void new criarColecao() {
+		var colecao = new Colecao();
+		this.colecoes.add(colecao);
+	}
+	public void new adicionarFotoPerfil() {
+		FotoPerfil fotoPerfil = new FotoPerfil();
+		this.fotoPerfil.add(fotoPerfil);
+	}
+	
+
 }
