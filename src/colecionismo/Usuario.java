f0ee91520932ef3f0204743d38e6b9cb60dcff09
Lilian Scortegagna Ramos <lilianscortegagnaramos@gmail.com>
2026-07-16 21:41:40 -0300

    Desenvolvimento Usuario

--- src/colecionismo/Usuario.java
+++ src/colecionismo/Usuario.java
@@ -13,6 +13,22 @@ public class Usuario {
 	public ArrayList <Amigo> amigo = new ArrayList<Amigo>();
 	public Premium premium;
 	
+	public void setnomeUsuario (String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public void setEmail (String email) {
+		this.email = email;
+	}
+	public void setDataNascimento (String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public void setAreaInteresse (String areaInteresse) {
+		this.areaInteresse = areaInteresse;
+	}
+	public void setTelefone (int telefone) {
+		this.telefone = telefone;
+	}
+	
 	public void setCriarColecao (Colecao colecao) {
 		
 	}
