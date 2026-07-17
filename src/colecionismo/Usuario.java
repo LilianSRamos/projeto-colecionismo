be834a2b5f31dd137cb5b6abae7673e48d8a5a0b
Lilian Scortegagna Ramos <lilianscortegagnaramos@gmail.com>
2026-07-16 21:23:20 -0300

    Novas Classes e desenvolvimento de Usuario

--- src/colecionismo/Usuario.java
+++ src/colecionismo/Usuario.java
@@ -1,5 +1,29 @@
 package colecionismo;
+import java.util.ArrayList;
 
 public class Usuario {
-
+	
+	private String nomeUsuario;
+	private String email;
+	private String dataNascimento;
+	private int telefone;
+	private String areaInteresse;
+	public ArrayList <Colecao> colecao = new ArrayList<Colecao>();
+	public FotoPerfil fotoPerfil;
+	public ArrayList <Amigo> amigo = new ArrayList<Amigo>();
+	public Premium premium;
+	
+	public void setCriarColecao (Colecao colecao) {
+		
+	}
+	public void setAdicionarFotoPerfil (FotoPerfil fotoPerfil) {	
+		
+	}
+	
+	public void setAdicionarAmigo (Amigo amigo) {
+		
+	}
+	public void setVirarPremium (Premium premium) {
+		
+	}
 }
